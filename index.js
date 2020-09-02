f81d37de4691be48fd1dffc81d0b3f7f8dc2df3f
Jack Steam <jacksteamdev@gmail.com>
2020-09-02 17:52:34 -0500

    Fix typo

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ module.exports = function (bundler) {
     }
   }
 
-  const writeAsset = (name, replacer = (x) => x) => {
+  const writeAsset = (name, replace = (x) => x) => {
     try {
-      const replaced = replacer(readAsset(name))
+      const replaced = replace(readAsset(name))
 
       fs.writeFileSync(name, replaced)
     } catch (error) {
-      logger.error('there was an error in your replacer')
+      logger.error('there was an error in your replace')
       throw e
     }
   }
@@ -42,14 +42,14 @@ module.exports = function (bundler) {
   bundler.on('bundled', async (bundle) => {
     try {
       const CWD = process.cwd()
-      const processFn = require(path.join(CWD, '.parcel-plugin-replacer.js'))
+      const processFn = require(path.join(CWD, '.parcel-plugin-replace.js'))
 
       if (processFn && typeof processFn === 'function') {
         await processAsset(bundle, processFn)
       }
     } catch (error) {
       logger.warn(
-        'parcel-plugin-replace cannot work without a .parcel-plugin-replacer.js in the root of your project!'
+        'parcel-plugin-replace cannot work without a .parcel-plugin-replace.js in the root of your project!'
       )
     }
   })
